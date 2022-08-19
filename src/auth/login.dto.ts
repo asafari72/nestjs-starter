58e8df747df743e8eca98b393f4800025414648d
asafari72 <asafari72@gmail.com>
2022-08-20 01:35:27 +0430

    login

--- src/auth/login.dto.ts
+++ src/auth/login.dto.ts
@@ -1,4 +1,10 @@
-export interface LoginDTO {
+import { ArrayMinSize, IsArray, IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class LoginDTO {
+  @IsString()
+  @IsNotEmpty()
   username: string;
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
